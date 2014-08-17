84bdc19cff1d36c2c097eac28e45bee79eb0c326
leehoawki <leehoawki@gmail.com>
2014-08-17 18:59:35 +0800

    applier and filter

--- py/pythonpy.py
+++ py/pythonpy.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python
 import sys
 import re
-
-def myeval(exp):
-	myimport(exp)
-	resp = eval(exp)
-	if isinstance(resp,list):
-		for o in resp:
-			print o
-	else:
-		print resp
+import argparse
 
 def myimport(exp):
 	matches = re.findall(r"([a-zA-Z_][a-zA-Z0-9_]*)\.", exp)
@@ -20,7 +12,50 @@ def myimport(exp):
 		except ImportError as e:
 			pass
 
-if __name__=='__main__':
-	if len(sys.argv) == 2 :
-		myeval(sys.argv[1])
+def evaluate(exp):
+	myimport(exp)
+	resp = eval(exp)
+	if isinstance(resp,list):
+		for o in resp:
+			print o
+	else:
+		print resp
 
+def apply(stdin,exp):
+	myimport(exp)
+	for line in stdin:
+		x = stdinparse(line)
+		print eval(exp)
+	
+
+def filter(stdin,exp):
+	myimport(exp)
+	for line in stdin:
+		x = stdinparse(line)
+		resp = eval(exp)
+		if resp:
+			print x
+
+def stdinparse(line):
+	if line[-1] == '\n':
+		return line[0:-1]
+
+if __name__=='__main__':
+	parser = argparse.ArgumentParser()
+
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument('-x', action='store_true', help='execute lambda expression using stdin as x')
+	group.add_argument('-f', action='store_true', help='filter the stdin using a lambda expression')
+	
+	parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+	parser.add_argument('exp', action="store")
+	arguments = parser.parse_args()
+	
+	if arguments.x:
+		apply(sys.stdin,arguments.exp)
+		exit(0)
+	if arguments.f:
+		filter(sys.stdin,arguments.exp)
+		exit(0)
+	
+	evaluate(arguments.exp)
