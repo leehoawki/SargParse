73a5ff4e3e14ccd68e541b00b7fc0e4f53997412
lee haoqi <leehaoqi@lees-MacBook-Air.local>
2014-08-17 22:54:43 +0800

    reduce function

--- py/pythonpy.py
+++ py/pythonpy.py
@@ -36,9 +36,23 @@ def filter(stdin,exp):
 		if resp:
 			print x
 
+def reduce(stdin,exp):
+	myimport(exp)
+	count = 0
+	for line in stdin:
+		if count == 0:
+			y=stdinparse(line)
+		else:
+			x=stdinparse(line)
+			y=eval(exp)
+		count+=1
+	print y
+
 def stdinparse(line):
 	if line[-1] == '\n':
 		return line[0:-1]
+	else:
+		return line
 
 if __name__=='__main__':
 	parser = argparse.ArgumentParser()
@@ -46,6 +60,7 @@ if __name__=='__main__':
 	group = parser.add_mutually_exclusive_group()
 	group.add_argument('-x', action='store_true', help='execute lambda expression using stdin as x')
 	group.add_argument('-f', action='store_true', help='filter the stdin using a lambda expression')
+	group.add_argument('-r', action='store_true', help='reduce the stdin using expression of x and y')
 	
 	parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 	parser.add_argument('exp', action="store")
@@ -57,5 +72,8 @@ if __name__=='__main__':
 	if arguments.f:
 		filter(sys.stdin,arguments.exp)
 		exit(0)
-	
+	if arguments.r:
+		reduce(sys.stdin,arguments.exp)
+		exit(0)
+
 	evaluate(arguments.exp)
