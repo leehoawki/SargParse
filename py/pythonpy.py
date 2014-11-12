6467c316f003def3aea8619029e1cbaf27a7d2bd
leehoawki <leehoawki@gmail.com>
2014-11-12 12:30:17 +0800

    Update pythonpy.py

--- py/pythonpy.py
+++ py/pythonpy.py
@@ -3,37 +3,40 @@ import sys
 import re
 import argparse
 
-def myimport(exp):
-	matches = re.findall(r"([a-zA-Z_][a-zA-Z0-9_]*)\.", exp)
-	for module_name in matches:
-		try:
-			module = __import__(module_name)
-			globals()[module_name] = module
-		except ImportError as e:
-			pass
-
+def importmodule(func):
+	def wrapper(*args, **kw):
+		exp = args[0]
+		matches = re.findall(r"([a-zA-Z_][a-zA-Z0-9_]*)\.", exp)
+		for module_name in matches:
+			try:
+				module = __import__(module_name)
+				globals()[module_name] = module
+			except ImportError as e:
+				pass
+		return func(*args, **kw)
+	return wrapper
+			
+@importmodule
 def evaluate(exp):
-	myimport(exp)
 	resp = eval(exp)
 	printresp(resp)
-
-def apply(stdin,exp):
-	myimport(exp)
+	
+@importmodule
+def apply(exp,stdin):
 	for line in stdin:
 		x = stdinparse(line)
 		print eval(exp)
 	
-
-def filter(stdin,exp):
-	myimport(exp)
+@importmodule
+def filter(exp,stdin):
 	for line in stdin:
 		x = stdinparse(line)
 		resp = eval(exp)
 		if resp:
 			print x
 
-def reduce(stdin,exp):
-	myimport(exp)
+@importmodule
+def reduce(exp,stdin):
 	y = None
 	for line in stdin:
 		if y:
@@ -44,8 +47,8 @@ def reduce(stdin,exp):
 	if y:
 		print y
 
-def listp(stdin, exp):
-	myimport(exp)
+@importmodule
+def listp(exp,stdin):
 	x = map(stdinparse, list(stdin))
 	resp = eval(exp)
 	printresp(resp)
@@ -58,10 +61,7 @@ def printresp(resp):
 		print resp
 
 def stdinparse(line):
-	if line[-1] == '\n':
-		return line[0:-1]
-	else:
-		return line
+	return line[0:-1] if line[-1] == '\n' else line
 
 if __name__=='__main__':
 	parser = argparse.ArgumentParser()
@@ -77,16 +77,16 @@ if __name__=='__main__':
 	arguments = parser.parse_args()
 	
 	if arguments.x:
-		apply(sys.stdin, arguments.exp)
+		apply(arguments.exp,sys.stdin)
 		exit(0)
 	if arguments.f:
-		filter(sys.stdin, arguments.exp)
+		filter(arguments.exp,sys.stdin)
 		exit(0)
 	if arguments.r:
-		reduce(sys.stdin, arguments.exp)
+		reduce(arguments.exp,sys.stdin)
 		exit(0)
 	if arguments.l:
-		listp(sys.stdin, arguments.exp)
+		listp(arguments.exp,sys.stdin)
 		exit(0)
 
 	evaluate(arguments.exp)
